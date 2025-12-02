467d52637a2fa1960d16cbca3ca7f1d998a2039a
sergi <askj545@gmail.com>
2025-12-02 23:34:49 +0100

    Add day 2 in clojure

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ My solutions for Advent of Code 2025
 | Day |  Code | Language | Status | Thoughts |
 |-----|---|----------|--------|------|
 | Day 1 | [Clojure](day1/day1.clj) [Python](day1/day1.py)     | Clojure/Python  |    2x⭐    | Bruteforce was good enough for part2 |
+| Day 2 | [Clojure](day2/day2.clj)     | Clojure  |    2x⭐    | Go regex engine does not support back ref, back to clojure |
---
+++ day2/day2.clj
@@ -0,0 +1,49 @@
+
+(require '[clojure.java.io :as io])
+(require '[clojure.string :as str])
+(require 'clojure.main)
+
+(defn read-lines [path]
+  (with-open [r (io/reader path)]
+    (doall (line-seq r))))
+
+
+
+(defn invalid-id [id pattern]
+  (not (= nil (re-find (re-pattern pattern) (str id))))
+)
+
+(defn parse-data [raw]
+  (map #(str/split % #"-") (str/split raw #","))
+)
+
+
+(defn invalid-ids-in-range [[start end] pattern]
+
+  (let [
+    s (Long/parseLong start)
+    e (Long/parseLong end)
+  ]
+    (reduce + (filter #(invalid-id % pattern) (range s (inc e))))
+  )
+)
+
+(defn part-1 []
+  (let [
+    data (parse-data (first (read-lines "input.txt")))
+  ]
+   (reduce + (map #(invalid-ids-in-range % #"^(\d+)\1$") data ))
+  )
+)
+
+
+(defn part-2 []
+  (let [
+    data (parse-data (first (read-lines "input.txt")))
+  ]
+   (reduce + (map #(invalid-ids-in-range % #"^(\d+)\1+$") data ))
+  )
+)
+
+(println "Part 1: " (part-1))
+(println "Part 2: " (part-2))
