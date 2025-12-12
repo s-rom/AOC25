fdab01965165861973b9b46fbd20fbc19ae3c647
sergi <askj545@gmail.com>
2025-12-12 13:37:52 +0100

    Add smt2 generation for z3

--- day10/day10.go
+++ day10/day10.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"regexp"
 	"slices"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -24,6 +23,7 @@ type Button uint32 // Toggle mask
 type MachineSchema struct {
 	Goal                LightSet
 	ButtonMasks         []Button
+	ButtonValues        [][]int
 	JoltageRequirements []int
 }
 
@@ -83,23 +83,29 @@ func ParseLightSet(str string) LightSet {
 	return set
 }
 
-func ParseButtons(s string, totalLights int) []Button {
-	buttons := make([]Button, 0, 10)
+func ParseButtons(s string, totalLights int) ([]Button, [][]int) {
+	buttonMasks := make([]Button, 0, 10)
+	buttonValues := make([][]int, 0, 10)
 
 	tokens := strings.SplitSeq(s, " ")
 	for token := range tokens {
-		button := Button(0)
+		mask := Button(0)
 		// remove first and last => ( and )
 		token = token[1 : len(token)-1]
+
+		values := make([]int, 0, totalLights)
+
 		for numStr := range strings.SplitSeq(token, ",") {
 			num, _ := strconv.Atoi(numStr)
 			unsigned := uint32(1) << uint32(totalLights-1-num)
-			button = button | Button(unsigned)
+			mask = mask | Button(unsigned)
+			values = append(values, num)
 		}
-		buttons = append(buttons, button)
+		buttonValues = append(buttonValues, values)
+		buttonMasks = append(buttonMasks, mask)
 	}
 
-	return buttons
+	return buttonMasks, buttonValues
 }
 
 func ParseJoltage(s string) []int {
@@ -117,9 +123,12 @@ func ParseData(data []string) []MachineSchema {
 	for _, line := range data {
 		match := pattern.FindStringSubmatch(line)
 
+		masks, buttonValues := ParseButtons(match[2], len(match[1]))
+
 		machine := MachineSchema{
 			Goal:                ParseLightSet(match[1]),
-			ButtonMasks:         ParseButtons(match[2], len(match[1])),
+			ButtonMasks:         masks,
+			ButtonValues:        buttonValues,
 			JoltageRequirements: ParseJoltage(match[3]),
 		}
 		machines = append(machines, machine)
@@ -127,17 +136,7 @@ func ParseData(data []string) []MachineSchema {
 	return machines
 }
 
-// func key(slice []int) string {
-// 	slices.Sort(slice)
-// 	var builder strings.Builder
-// 	for _, n := range slice {
-// 		str := strconv.Itoa(n)
-// 		builder.WriteString(str)
-// 	}
-// 	return builder.String()
-// }
-
-func MinNumberOfPresses(m *MachineSchema) int {
+func MinNumberOfPressesLightGoal(m *MachineSchema) int {
 
 	minNumber := math.MaxInt
 	queue := make([][]int, 0, 10)
@@ -168,10 +167,12 @@ func MinNumberOfPresses(m *MachineSchema) int {
 		for buttonIndex := range m.ButtonMasks {
 
 			// Avoid pressing the last button again
+			// ==> unnecessary
 			if buttonIndex == front[len(front)-1] {
 				continue
 			}
 
+			// Avoid chosing a button already pushed
 			idx := slices.Index(front, buttonIndex)
 			if idx != -1 {
 				continue
@@ -186,12 +187,39 @@ func MinNumberOfPresses(m *MachineSchema) int {
 	return minNumber
 }
 
+func SliceEquals(s1 []int, s2 []int) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func GetJoltageValue(m *MachineSchema, buttonSeq []int) []int {
+
+	result := make([]int, len(m.JoltageRequirements))
+
+	for _, buttonIdx := range buttonSeq {
+		for _, joltageIndex := range m.ButtonValues[buttonIdx] {
+			result[joltageIndex]++
+		}
+	}
+
+	return result
+}
+
 func Part1(data []MachineSchema) uint64 {
 	total := uint64(0)
 	progress := 0
 	for _, m := range data {
 
-		n := MinNumberOfPresses(&m)
+		n := MinNumberOfPressesLightGoal(&m)
 		total += uint64(n)
 
 		progress++
@@ -200,8 +228,100 @@ func Part1(data []MachineSchema) uint64 {
 	return total
 }
 
-func Part2(data []string) uint64 {
+type EquationSet struct {
+	Equations    []Equation
+	OperandNames []string
+}
+
+type Equation struct {
+	Result   int
+	Operands []string
+}
+
+func GetEquations(m MachineSchema) EquationSet {
+	operandNames := make([]string, 0, len(m.JoltageRequirements))
+
+	for opIdx := range len(m.ButtonValues) {
+		// 'a' = 97
+		operandNames = append(operandNames, string(rune(97+opIdx)))
+	}
+
+	equations := make([]Equation, 0, 5)
+	for joltageIdx, joltage := range m.JoltageRequirements {
+		eq := Equation{
+			Result:   joltage,
+			Operands: make([]string, 0, len(m.JoltageRequirements)),
+		}
+
+		for buttonIndex, button := range m.ButtonValues {
+			idx := slices.Index(button, joltageIdx)
+			if idx != -1 { // Found
+				buttonName := operandNames[buttonIndex]
+				eq.Operands = append(eq.Operands, buttonName)
+			}
+		}
+
+		equations = append(equations, eq)
+	}
+
+	return EquationSet{
+		Equations:    equations,
+		OperandNames: operandNames,
+	}
+}
+
+func GenerateSMT2Code(eqs EquationSet) string {
+	var builder strings.Builder
+
+	builder.WriteString("(push 1)\n")
+
+	for _, opName := range eqs.OperandNames {
+		builder.WriteString("(declare-const ")
+		builder.WriteString(opName)
+		builder.WriteString(" Int)\n")
+	}
+
+	for _, opName := range eqs.OperandNames {
+		builder.WriteString("(assert (>= ")
+		builder.WriteString(opName)
+		builder.WriteString(" 0))\n")
+	}
+
+	for _, eq := range eqs.Equations {
+		builder.WriteString("(assert (= (+ ")
+		for _, opName := range eq.Operands {
+			builder.WriteString(opName)
+			builder.WriteString(" ")
+		}
+		builder.WriteString(") ")
+		builder.WriteString(strconv.Itoa(eq.Result))
+		builder.WriteString("))\n")
+	}
+
+	for _, opName := range eqs.OperandNames {
+		builder.WriteString("(minimize ")
+		builder.WriteString(opName)
+		builder.WriteString(")\n")
+	}
+
+	builder.WriteString("(check-sat)\n(get-value (")
+
+	for _, opName := range eqs.OperandNames {
+		builder.WriteString(opName)
+		builder.WriteString(" ")
+	}
+
+	builder.WriteString("))\n")
+
+	builder.WriteString("(pop 1)\n")
+
+	return builder.String()
+}
+
+func Part2(data []MachineSchema) uint64 {
 	total := uint64(0)
+
+	// TODO: Solve using z3 library
 
 	return total
 }
@@ -215,22 +335,17 @@ func main() {
 
 	rawData := readData(filePath)
 	machines := ParseData(rawData)
-	// m1 := machines[0]
-	// set := LightSet(0)
-	// set = PressButton(set, m1.ButtonMasks[0])
-	// set = PressButton(set, m1.ButtonMasks[1])
-	// set = PressButton(set, m1.ButtonMasks[2])
 
-	// fmt.Println(strconv.FormatInt(int64(set), 2))
+	// var part1 uint64 = Part1(machines)
+	// var part2 uint64 = Part2(machines)
+
+	// fmt.Printf("Part 1: %d\nPart 2: %d\n", part1, part2)
 
 	for _, m := range machines {
-		sort.Slice(m.ButtonMasks, func(i, j int) bool {
-			return m.ButtonMasks[j] > m.ButtonMasks[i]
-		})
+		eq := GetEquations(m)
+		fmt.Println(GenerateSMT2Code(eq))
 	}
 
-	var part1 uint64 = Part1(machines)
-	var part2 uint64 = Part2(rawData)
+	// fmt.Println(eq)
 
-	fmt.Printf("Part 1: %d\nPart 2: %d\n", part1, part2)
 }
